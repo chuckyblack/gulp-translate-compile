2a8727365f641dce6446ce27a07d869977abe6f7
Jaromír Pufler <jaromir.pufler@gmail.com>
2018-06-30 12:09:32 +0200

    refactor using ES6 class

--- index.js
+++ index.js
@@ -3,86 +3,115 @@ const fs = require('fs');
 const pofile = require('pofile');
 const cheerio = require('cheerio');
 
-function hasAttr(element, attrName) {
-	const attr = element.attr(attrName);
-	return typeof attr !== typeof undefined && attr !== false
-}
+class Translator {
+	constructor(path, attributes, throwOnMissingTranslation, throwOnEmptyTranslation) {
+		this.path = path;
+		this.attributes = attributes;
+		this.throwOnMissingTranslation = throwOnMissingTranslation;
+		this.throwOnEmptyTranslation = throwOnEmptyTranslation;
+		this.msgStrById = {};
+		this.loadPoFile(path);
+	}
 
-function normalizeText(text) {
-	return text
-		.replace("\n", " ")
-		.replace("\t", " ")
-		.replace(/\s+/g, ' ')
-		.replace("/>", ">")
-		.trim();
-}
+	loadPoFile(path) {
+		const data = fs.readFileSync(path, 'utf-8');
+		const po = pofile.parse(data);
+		po.items.forEach(item => {
+			if (item.msgstr[0] === "") {
+				return;
+			}
+			// TODO: map file path too
+			this.msgStrById[item.msgid] = item.msgstr[0];
+		});
+	}
 
-function translateAttr(element, attrName, msgstrByMsgid) {
-	const newText = msgstrByMsgid[element.attr(attrName)];
-	element.attr(attrName, newText);
-}
-
-module.exports = function(options) {
-	options = Object.assign({
-		attributes: [],
-		throwOnMissingTranslation: true,
-		throwOnEmptyTranslation: true
-	}, options);
-	const data = fs.readFileSync(options.pofile, 'utf-8');
-	const po = pofile.parse(data);
-	let msgstrById = {};
-
-	po.items.forEach(function(item){
-		if (item.msgstr[0] === ""){
-			return;
-		}
-		msgstrById[item.msgid] = item.msgstr[0];
-	});
-
-	return through.obj(function (file, enc, cb) {
-		const fileContents = file.contents.toString();
-		const $ = cheerio.load(fileContents, {
+	translate(file) {
+		const content = file.contents.toString();
+		const $ = cheerio.load(content, {
 			decodeEntities: false
 		});
-		$("*").each(function() {
-			const element = $(this);
-			if (hasAttr(element, "i18n")) {
-				const elementText = normalizeText(element.html());
+		$("*").each((index, element) => {
+			element = $(element);
+			if (this.hasAttr(element, "i18n")) {
+				const elementText = this.normalizeText(element.html());
 				if (elementText === "") {
 					// valid state - element has no content, eg. <input>
 					return;
 				}
-				const translatedText = msgstrById[elementText];
-				if (options.throwOnMissingTranslation && translatedText === undefined) {
-					throw (
-						"Source string is missing in " + options.pofile + "!\n" +
-						"translated file " + file.path + "\n" +
-						"elementText = '" + elementText + "'\n" +
-						"translatedText = '" + translatedText + "'"
-					);
-				}
-				if (options.throwOnEmptyTranslation && translatedText === "") {
-					throw (
-						"The text has empty translation in " + options.pofile + "!\n" +
-						"translated file " + file.path + "\n" +
-						"elementText = '" + elementText + "'\n" +
-						"translatedText = '" + translatedText + "'"
-					);
-				}
+				const translatedText = this.getTranslatedText(file, elementText);
 				element.text(translatedText);
 				element.removeAttr("i18n");
 			}
 
-			options.attributes.forEach(function (attrName){
-				if (hasAttr(element, attrName)) {
-					translateAttr(element, attrName, msgstrById);
+			this.attributes.forEach(attrName => {
+				if (this.hasAttr(element, attrName)) {
+					this.translateAttr(file, element, attrName);
 				}
 			});
 		});
-		file.contents = Buffer.from($.html());
+		return $.html();
+	}
+
+	getTranslatedText(file, original) {
+		const translatedText = this.msgStrById[original];
+		if (this.throwOnMissingTranslation && translatedText === undefined) {
+			throw this.getErrorMessage("Source string is missing", file, original, translatedText);
+		}
+		if (this.throwOnEmptyTranslation && translatedText === "") {
+			throw this.getErrorMessage("The text has empty translation", file, original, translatedText);
+		}
+		return translatedText;
+	}
+
+	getErrorMessage(message, file, originalText, translatedText) {
+		return (
+			message +
+			" in " + this.path + "!\n" +
+			"translated file " + file.path + "\n" +
+			"originalText = '" + originalText + "'\n" +
+			"translatedText = '" + translatedText + "'"
+		);
+	}
+
+	normalizeText(text) {
+		return text
+			.replace("\n", " ")
+			.replace("\t", " ")
+			.replace(/\s+/g, ' ')
+			.replace("/>", ">")
+			.trim();
+	}
+
+	hasAttr(element, attrName) {
+		const attr = element.attr(attrName);
+		return typeof attr !== typeof undefined && attr !== false;
+	}
+
+	translateAttr(file, element, attrName) {
+		const attrText = element.attr(attrName);
+		const translatedText = this.getTranslatedText(file, attrText);
+		element.attr(attrName, translatedText);
+	}
+}
+
+function getDefault(value, defaultValue) {
+	return value !== undefined ? value : defaultValue;
+}
+
+module.exports = function(options) {
+	const translator = new Translator(
+		options.pofile,
+		options.attributes || [],
+		getDefault(options.throwOnMissingTranslation, true),
+		getDefault(options.throwOnEmptyTranslation, true)
+	);
+
+	return through.obj(function(file, enc, callback) {
+		const translated = translator.translate(file);
+		file.contents = Buffer.from(translated);
 		this.push(file);
-		cb();
-	}, function (cb) {
-		cb();
+		callback();
+	}, function(callback) {
+		callback();
 	});
 };
